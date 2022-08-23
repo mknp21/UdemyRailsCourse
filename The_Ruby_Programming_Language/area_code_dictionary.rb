fc52f88d3bfa4bc9bdfc39087fd36072a02ad796
Mika Patterson <mknpatt21@gmail.com>
2022-08-22 18:09:08 -0700

    Create an area code dictionary program

--- The_Ruby_Programming_Language/area_code_dictionary.rb
+++ The_Ruby_Programming_Language/area_code_dictionary.rb
@@ -13,15 +13,24 @@ dial_book = {
  
 # Get city names from the hash
 def get_city_names(somehash)
-# Write code here
+  somehash.keys
 end
  
 # Get area code based on given hash and key
 def get_area_code(somehash, key)
-# Write code here
+ puts "The area code for #{key} is #{somehash[key]}"
 end
  
 # Execution flow
 loop do
-# Write your program execution code here
+  puts "Do you want to lookup an area code based on a city name?(Y/N)"
+  answer = gets.chomp.downcase
+  break if answer != "y"
+
+  puts "Which city do you want the area code for"
+  puts get_city_names(dial_book)
+
+  puts "Enter your selection"
+  city_selection = gets.chomp
+  get_area_code(dial_book, city_selection)
 end
